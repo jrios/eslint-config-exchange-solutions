c9f184c6fef92f28e6e77c87d7d47b6f22e3ae00
Matthew Smith <mtscout6@gmail.com>
2015-12-01 08:34:30 -0700

    Update tests for updated dependencies

--- test/samples/index/no-use-before-define.bad.js
+++ test/samples/index/no-use-before-define.bad.js
@@ -11,6 +11,6 @@ function g() {
 }
 const b = 1;
 
-// error 3:7 no-use-before-define - a was used before it was defined
-// error 6:1 no-use-before-define - f was used before it was defined
-// error 10:10 no-use-before-define - b was used before it was defined
+// error 3:7 no-use-before-define - "a" was used before it was defined
+// error 6:1 no-use-before-define - "f" was used before it was defined
+// error 10:10 no-use-before-define - "b" was used before it was defined
