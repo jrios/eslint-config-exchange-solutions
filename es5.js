bfba8f1b4f95b0d56a4142735a71698cb4eb002e
Matt Smith <mtscout6@gmail.com>
2016-01-08 15:39:06 -0700

    Move to dependency instead of peerDependency

--- es5.js
+++ es5.js
@@ -2,5 +2,5 @@ module.exports = {
   extends: [
     'eslint-config-airbnb/legacy',
     'eslint-config-exchange-solutions/core'
-  ]
+  ].map(require.resolve)
 };
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
   extends: [
     'eslint-config-airbnb',
     'eslint-config-exchange-solutions/base'
-  ]
+  ].map(require.resolve)
 };
